f88451023f5fcf7e8158edf866c22f5b5f010a56
Eriksiano <70276150+Eriksiano@users.noreply.github.com>
2020-09-26 14:42:44 -0400

    Add files via upload

---
+++ statistics.h
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void start_log();
+void log_statistics(int id, int server, int arrival_time, int pop_time, int finished_time,
+                    int response_time, int process_time, int queue_len);
+void calculate_statistics(int INIT_TIME, int FIN_TIME);
+
